25ef041d59580c6888e7b32e6b4fd442dd9d4b0d
agussanguesa32 <agussanguesa@gmail.com>
2022-10-17 18:51:06 -0300

    Agregado delay a la splash screen

--- app/src/main/java/com/agus/proyectointegradortm/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/agus/proyectointegradortm/activities/SplashScreenActivity.kt
@@ -1,12 +1,17 @@
 package com.agus.proyectointegradortm.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.Window
 import android.view.WindowManager
 import com.agus.proyectointegradortm.R
 
+
 class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 1500 // 1 sec
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -14,6 +19,15 @@ class SplashScreenActivity : AppCompatActivity() {
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
-        setContentView(R.layout.activity_splash_screen)
+
+        //Pasamos al login
+        Handler().postDelayed({
+
+            startActivity(Intent(this,LoginActivity::class.java))
+
+            finish()
+        }, SPLASH_TIME_OUT)
+
     }
+
 }
