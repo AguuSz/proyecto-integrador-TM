4d0b22ad34f2db407d222958909366f5b85f13b5
agussanguesa32 <agussanguesa@gmail.com>
2022-10-17 18:32:08 -0300

    Agregado el logo y background a la splash screen

--- app/src/main/java/com/agus/proyectointegradortm/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/agus/proyectointegradortm/activities/SplashScreenActivity.kt
@@ -2,11 +2,18 @@ package com.agus.proyectointegradortm.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.agus.proyectointegradortm.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        // Escondemos la action bar
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
         setContentView(R.layout.activity_splash_screen)
     }
 }
