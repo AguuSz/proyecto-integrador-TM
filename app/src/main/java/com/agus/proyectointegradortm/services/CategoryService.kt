001e4618bcc46cab672c07a0ac16a5fafa0a5d17
AguuSz <agus.sepu2000@hotmail.com>
2022-11-22 16:18:27 -0300

    minor change

--- app/src/main/java/com/agus/proyectointegradortm/services/CategoryService.kt
+++ app/src/main/java/com/agus/proyectointegradortm/services/CategoryService.kt
@@ -5,6 +5,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface CategoryService {
-    @GET("v1/4cea5c4b-fdae-4a8f-b1e3-e7aecf49b11d")
+    @GET("v1/72ee4c05-dbdc-4b99-8509-9cc27e9f6cb5")
     fun getCategories(): Call<Category>
 }
